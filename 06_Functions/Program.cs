8c192f500edf32e17157fd1b9c8c09b658179d65
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-12-05 19:22:05 +0300

    Some changes

---
+++ 06_Functions/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _06_Functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Metinsel Fonksiyonlar/Metotlar
+
+            string ns1,ns1_1;
+            string ns2,ns2_1;
+
+            // ToUpper,ToLower
+            ns1 = "ümit karaçivi...";
+            ns1_1=ns1.ToLower();
+            ns2 = "DOĞA BENGİ KARAÇİVİ";
+            ns2_1=ns2.ToLower();
+
+            Console.WriteLine(ns1 + " " +ns1_1 + "\n");
+            Console.WriteLine(ns2 + " " + ns2_1 + "\n");
+
+            // concat : stringleri birleitiriyor.
+
+
+            Console.WriteLine("Benim ve kızımın adları : " +);
+
+
+            Console.ReadKey();
+        }
+    }
+}
