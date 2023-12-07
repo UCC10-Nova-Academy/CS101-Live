5fed8fd5eb978c0e9d95b1f7daf7497582d496ab
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-12-07 19:29:35 +0300

    Some changes

--- 07_Arrays/Program.cs
+++ 07_Arrays/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Dizi tanımlama,değer atama ve kullanma
+
+            // 20 elemanlı int değerler tutan "sayilar" isimli bir dizi tanımlama
+            int[] sayilar = new int[20]; // tüm içerik 0
+
+            // 5.index indeki elemanına 5 değeri koy
+
+            sayilar[5] = 5;
+
+            Console.WriteLine("5. indexdeki değer = {0}", sayilar[5]);
+
+
+
+
+            #endregion
+
+            Console.ReadKey();
         }
     }
 }
