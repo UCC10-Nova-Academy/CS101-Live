7d2f131496e8e0ed86c45a4542aee9fa60ef94ec
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-12-07 20:40:05 +0300

    Some changes

--- 07_Arrays/Program.cs
+++ 07_Arrays/Program.cs
@@ -6,41 +6,57 @@
         {
             #region Dizi tanımlama,değer atama ve kullanma
 
-            // 20 elemanlı int değerler tutan "sayilar" isimli bir dizi tanımlama
-            int[] sayilar = new int[20]; // tüm içerik 0
+            //// 20 elemanlı int değerler tutan "sayilar" isimli bir dizi tanımlama
+            //int[] sayilar = new int[20]; // tüm içerik 0
 
-            // 5.index indeki elemanına 5 değeri koy
+            //// 5.index indeki elemanına 5 değeri koy
 
-            sayilar[5] = 5;
+            //sayilar[5] = 5;
 
-            Console.WriteLine("5. indexdeki değer = {0}", sayilar[5]);
+            //Console.WriteLine("5. indexdeki değer = {0}", sayilar[5]);
 
-            // Şöyle de olabilir
-            string[] dizi1 = { "Bir", "İki", "Üç" }; // daha yaratırken içine veriyi yerleştiriyorum.
-            int[] dizi2 = {2,-4,5,6};
+            //// Şöyle de olabilir
+            //string[] dizi1 = { "Bir", "İki", "Üç" }; // daha yaratırken içine veriyi yerleştiriyorum.
+            //int[] dizi2 = {2,-4,5,6};
 
-            float[] dizi3 = { 2f, 2.5f, 4.1f }; // yaratırken ilk değerleri atama
+            //float[] dizi3 = { 2f, 2.5f, 4.1f }; // yaratırken ilk değerleri atama
 
-            //Not : yukardaki kullanımı ancak ve ancak dizi daha ilk tanımlanırken yapabilir. Sonrasında bçyle bir atama yapamayız.
+            ////Not : yukardaki kullanımı ancak ve ancak dizi daha ilk tanımlanırken yapabilir. Sonrasında bçyle bir atama yapamayız.
 
-            //int[] dizi4;
+            ////int[] dizi4;
 
-            //dizi4 = { 1,2,3};
+            ////dizi4 = { 1,2,3};
 
-            // Dinamik dizi yaratımı
-            Console.WriteLine("Lütfen bir değer giriniz :");
-            int boyut = Convert.ToInt32(Console.ReadLine());// yaratılacak olan dizinin boyutu isteniyor aslında
-            int[] dizi5 = new int[boyut]; // dinamik bir dizi yaratımı
-            dizi5[0] = 130;
-            dizi5[1] = 20;
-            dizi5[2] = 40;
+            //// Dinamik dizi yaratımı
+            //Console.WriteLine("Lütfen bir değer giriniz :");
+            //int boyut = Convert.ToInt32(Console.ReadLine());// yaratılacak olan dizinin boyutu isteniyor aslında
+            //int[] dizi5 = new int[boyut]; // dinamik bir dizi yaratımı
+            //dizi5[0] = 130;
+            //dizi5[1] = 20;
+            //dizi5[2] = 40;
 
-            // bu dizinin tüm elemanlarını ekrana bastırınız.
+            //// bu dizinin tüm elemanlarını ekrana bastırınız.
 
-            for (int index = 0;index <= dizi5.Length-1; index++)
+            //for (int index = 0;index <= dizi5.Length-1; index++)
+            //{
+            //    Console.WriteLine("Dizinin {0}. elemanı {1} dir...",index, dizi5[index] );
+            //}
+            #endregion
+
+            #region for/foreach döngüleri
+            // Bir tamsayı dizimin tüm elemanlarını toplayan durum
+
+            int[] sayilar1 = { 1, 2, 3,4,5,6,7,8,9,10 };
+
+            // toplamı isteniyor
+            int toplam = 0;
+
+            for (int index = 0;index <= sayilar1.Length-1; index++)
             {
-                Console.WriteLine("Dizinin {0}. elemanı {1} dir...",index, dizi5[index] );
+                toplam = toplam + sayilar1[index];
             }
+
+            Console.WriteLine("Dizi toplamı {0} ",toplam);
 
 
             #endregion
