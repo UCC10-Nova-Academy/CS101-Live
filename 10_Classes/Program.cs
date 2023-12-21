2ee14fd7e75919e6c8abeeb1371cdda299012d77
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-12-21 19:41:04 +0300

    some changes

---
+++ 10_Classes/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _10_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Sınıflar nerelerde oluşturulabılır.
+
+            // bulunulan namespace içinde
+            // baska bir namespace içinde/yani bulunulan ns nin dışında)
+            // herhangi bir class içinde de yaratılabilir.
+
+
+            #endregion
+
+            #region Personel 
+
+            // bir personel kayıt sistemi oluşturulacak
+            // yani class tanımlamamı ayrı bir dosyada (cs) yaratacağım.
+
+
+
+
+
+            #endregion
+
+
+
+
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ 10_Classes/clsPersonel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_Classes
+{        
+    // Burası artık bir sınıf
+        //bunun içine bazı tanımlamalar yapacagız...property(ozellık)...değisgen
+
+        // 1. bir personelin kayıt yapısını ..kaydını oluşturmak için bir metot yaratabiliriz ve bunun içinde cesitli güncelleme ıslemlerinin olacağı bir yapı kurabiliriz.
+
+    internal class clsPersonel
+    {
+        // değisgenlerimi tanımlıyorum
+        // public ...dış dunyaya acık
+        public int PersonelID; // Personel no
+        public string Ad;
+        public string Soyad;
+        public string Unvan = null;
+        public int Maas;
+        public int IzinGunu;
+
+        // 1.
+        void setPersonelBilgileri(string pAd,string pSoyad,int pMaas) // parametrik
+        {
+            // Kural 
+            // eğer gelen maas bilgisi 10000 PNG Kina dan büyük ve esitse izin gunu 14 gun olsun
+            // 10000 - 14000 dahil ise unvan kıdemli pers izin gunu 20
+            // 14001 - 15500 dahil Kıdemli+ pers izin gunu 24 gun
+            // 15501 butukse unvan uzman pers. ig 30 gün
+
+            this.Ad = pAd;
+            this.Soyad = pSoyad;
+            this.Maas = pMaas;
+            this.IzinGunu = 14;
+
+            if ( pAd.Length > 0 && pSoyad.Length > 0)
+            {
+                if(pMaas >= 10000)
+                {
+                    if(pMaas >=10000 && pMaas <= 14000)
+
+                }
+            }
+        }
+
+
+
+
+
+
+
+    }
+}
