8a1e777c3588b290f6d949e48b72b2a6a8bdad97
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2023-12-28 19:23:39 +0300

    Some changes

---
+++ 11_AccessModifiers/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    // Access Modifiers : Erişim Belirleyiciler
+
+    private static void Main(string[] args)
+    {
+        // bir dış class a erişerek ilgili bazı bilgileri güncelle vs vs
+        // Bir clsTutorials isimli bir classımız olsun ama ayrı dosya içinde
+        // o classın icersinde yazılmış olan bir metoda bulunduğumuz yerden erişip kullanma
+
+
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ 11_AccessModifiers/clsTutorials.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_AccessModifiers
+{
+    // 3 tane metot olacak bu sınıfın içinde
+    // 1. üzerine gelen parametrelere göre bu class içinde tanımlı olan TutorialID(int) ve TutorialName(string) deüisgenlerin değerini atasın . setTutorial
+    // 2.Bana en son girilen bilginin TutorialName.. bilgisini getirsin .. getTutorial
+    // 3. yeni bazı değerleri eskisi üzerine yazsın (change etsin).. chgTutorial 
+
+    // Yani bu yazılacak metotları ana programdan (Program.cs) çağıracak/kullanacak şekilde ayarlayın
+
+
+
+
+    internal class clsTutorials
+    {
+    }
+}
