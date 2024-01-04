614c1c318fcaab38e16ec608582ae4026a79969e
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-01-04 19:17:13 +0300

    Some changes

--- 12_Inheritance/Program.cs
+++ 12_Inheritance/Program.cs
@@ -22,14 +22,21 @@
         #region Sample1
         // bir Person isimli bir üst sınıfımız olsun.Ve bundan 3 adet Employee,Student,Customer alt sınıf olusturalım
 
+        Employee employee = new Employee(); // employee nesnem yaratıldı.
+
+        employee.Id = 1;
+        employee.FName = "Ümit";
+        employee.LName = "Karaçivi";
+        employee.CompanyName = "Oyak Renault";
+
+        string FullName=employee.getFullInfo();
+
+        Console.WriteLine($"Personel Bilgisi : \n\nPersonel No : {employee.Id}\nPersonel Ad Soyad : {FullName}\nÇalıştığı şirket : {employee.CompanyName}");
+
 
 
 
         #endregion
-
-
-
-
 
         Console.ReadKey();
     }
@@ -40,7 +47,7 @@
         public string FName { get; set; } // First Name property(alan)
         public string LName { get; set; } // Last Name
 
-        public string getFullName()
+        public string getFullInfo()
         {
             return FName + " " + LName;
         }
@@ -51,6 +58,9 @@
     class Employee : Person // Kendini Person sıfından kalıttı
     {
         public int Id { get; set; }
+
         public string CompanyName { get; set; }
+
+        
     }
 }
