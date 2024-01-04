2d6ca50988cf419476c34f8ccb5562c88a3f45ec
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-01-04 18:50:35 +0300

    Some changes

---
+++ 12_Inheritance/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    // Inheritance - Kalıtım/Miras
+
+    // OOP en önemli kısımlarından biri
+    // Var olan, yaratılmış bir sınıftan bir diğer sınıfa tüm özelliklerin aktarımı
+    // Bazı terminolojiler
+    // SuperClass.. üst sınıf.çmetotlarını ve özelliklerini baska bir sınıfa miras bırakan aktaran sınıflar
+
+    // SubClass .. alt sınıf..diğer bir sınıfın özelliklerini  alan sınıftır.
+
+    private static void Main(string[] args)
+    {
+        // Kalıtım olayı genel olarak 5 çesit diyebiliriz.
+
+        // 1.Single
+        // 2.MultiLevel
+        // 3.Hierarchical
+        // 4.Multiple (Interface yöntemiyle)
+        &&
+
+
+
+
+
+        Console.ReadKey();
+    }
+}
