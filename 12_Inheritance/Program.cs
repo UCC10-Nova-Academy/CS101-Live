79c0076175662075ff16e1ed8f2296eb00814570
Ümit KARAÇİVİ <umit.karacivi@outlook.com>
2024-01-09 18:57:09 +0300

    Some changes

--- 12_Inheritance/Program.cs
+++ 12_Inheritance/Program.cs
@@ -11,7 +11,7 @@
 
     private static void Main(string[] args)
     {
-        // Kalıtım olayı genel olarak 5 çesit diyebiliriz.
+        // Kalıtım olayı genel olarak 4 çesit diyebiliriz.
 
         // 1.Single
         // 2.MultiLevel
@@ -22,17 +22,21 @@
         #region Sample1
         // bir Person isimli bir üst sınıfımız olsun.Ve bundan 3 adet Employee,Student,Customer alt sınıf olusturalım
 
-        Employee employee = new Employee(); // employee nesnem yaratıldı.
+        //Employee employee = new Employee(); // employee nesnem yaratıldı.
 
-        employee.Id = 1;
-        employee.FName = "Ümit";
-        employee.LName = "Karaçivi";
-        employee.CompanyName = "Oyak Renault";
+        //employee.Id = 1;
+        //employee.FName = "Ümit";
+        //employee.LName = "Karaçivi";
+        //employee.CompanyName = "Oyak Renault";
 
-        string FullName=employee.getFullInfo();
+        //string FullName=employee.getFullInfo();
 
-        Console.WriteLine($"Personel Bilgisi : \n\nPersonel No : {employee.Id}\nPersonel Ad Soyad : {FullName}\nÇalıştığı şirket : {employee.CompanyName}");
+        //Console.WriteLine($"Personel Bilgisi : \n\nPersonel No : {employee.Id}\nPersonel Ad Soyad : {FullName}\nÇalıştığı şirket : {employee.CompanyName}");
 
+        Dog dog = new Dog();
+        
+        dog.Eat(); // super clss içindeki metodu kullanabiliyorum
+        dog.HavHav(); // kendi içindeki metodu kullanabiiyorıum.
 
 
 
@@ -57,6 +61,7 @@
         public string FName { get; set; } // First Name property(alan)
         public string LName { get; set; } // Last Name
 
+        // super class içindeki bir metot
         public string getFullInfo()
         {
             return FName + " " + LName;
@@ -64,8 +69,17 @@
 
     }
 
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Hayvanlar yerler....");
+        }
+
+    }
+
     // Sub Class
-    class Employee : Person // Kendini Person sıfından kalıttı
+    class Employee : Person // Kendini Person sıfından kalıttı/miras aldım
     {
         public int Id { get; set; }
 
@@ -73,9 +87,32 @@
 
         
     }
+
+    class Dog : Animal
+    {
+        public void HavHav()
+        {
+            Console.WriteLine("Köpekler havlar....");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public void MiyavMiyav()
+        {
+            Console.WriteLine("Kediler de doyndum emeeen derler");
+        }
+    }
+
+
+
     #endregion
 
     #region 2.Multilevel Inheritance
+
+
+
+
 
     #endregion
 
